e26a100d54672a4834ac8fc0618751dfb6810b14
Lara Inskip <larahy@gmail.com>
2018-09-13 11:35:49 -0400

    Fix bottom navigation to bottom of page

--- src/components/Navigation/Footer.js
+++ src/components/Navigation/Footer.js
@@ -2,15 +2,15 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faPlusSquare} from '@fortawesome/free-regular-svg-icons'
-import {faFingerprint, faGlobe} from '@fortawesome/free-solid-svg-icons'
+// import {faPlusSquare, faUser} from '@fortawesome/free-regular-svg-icons'
+import {faHome, faUser, faPlusSquare} from '@fortawesome/free-solid-svg-icons'
 import './navigation.scss'
 
 export default class Footer extends React.Component {
 
     render() {
         return (
-            <footer className="footer">
+            <nav id="navbarBottom" className="navbar is-fixed-bottom has-shadow footer">
                 <div className="columns is-mobile">
                     <div className="column left-icon">
                         <Link to="/create/info">
@@ -19,16 +19,16 @@ export default class Footer extends React.Component {
                     </div>
                     <div className="column middle-icon">
                         <a>
-                            <span className="icon is-large"><FontAwesomeIcon icon={faFingerprint}/></span>
+                            <span className="icon is-large"><FontAwesomeIcon icon={faUser}/></span>
                         </a>
                     </div>
                     <div className="column right-icon">
                         <a>
-                            <span className="icon is-large"><FontAwesomeIcon icon={faGlobe}/></span>
+                            <span className="icon is-large"><FontAwesomeIcon icon={faHome}/></span>
                         </a>
                     </div>
                 </div>
-            </footer>
+            </nav>
 
         )
     }
