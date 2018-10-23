3b69a538c22408df53eee5b29d11aea5401128ab
Lara Inskip <larahy@gmail.com>
2018-10-23 08:33:00 -0400

    Minor style changes

--- src/components/Navigation/SimpleHeader.js
+++ src/components/Navigation/SimpleHeader.js
@@ -1,10 +1,11 @@
 import React, {Fragment} from 'react';
 import PropTypes from 'prop-types'
+import './navigation.scss'
 
 const SimpleHeader = (props) => (
     <Fragment>
-        <nav className="navbar">
-            <h1>{props.title}</h1>
+        <nav id="navbarTop"className="navbar has-text-centered">
+            <p className="title is-centered">{props.title}</p>
         </nav>
     </Fragment>
 )
