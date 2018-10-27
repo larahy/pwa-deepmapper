5f06ab951a3289e2d2ce1b3df0d6396bf77377d2
Lara Inskip <larahy@gmail.com>
2018-10-27 10:42:18 -0400

    Remove duplicate text from about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -16,10 +16,6 @@ const AboutPage = () => (
                 <h2 className="subtitle">Deepmappers maintain the quality of their deep map through a democratic process of voting - each submitted placecast must be majority-voted by the rest of the city's deepmappers. This system will also ensure diversity of content, subject-matter and perspectives.</h2>
                 <h2 className="subtitle">Each city's deep map will be enjoyed by visitors and citizens alike. It is a unique map of the city’s ‘inner geography’ – the stories, knowledge, observations and alternative perspectives that pervade all places. This human dimension of the city has so far been locked up inside the minds of its citizens. Deepmapper makes it publicly accessible for the first time.</h2>
                 <h2 className="subtitle">Launching in Autumn 2018, Deepmapper exemplifies technology at its most positive and important.</h2>
-                <h2 className="subtitle">Each city's community of deepmappers will be made up of citizens who are passionate about their city's history, architecture, politics, art, literature and local lore - both professionally (scholars, writers) and as amateurs.</h2>
-                <h2 className="subtitle">Deepmappers maintain the quality of their deep map through a democratic process of voting - each submitted placecast must be majority-voted by the rest of the city's deepmappers. This system will also ensure diversity of content, subject-matter and perspectives.</h2>
-                <h2 className="subtitle">Each city's deep map will be enjoyed by visitors and citizens alike. It is a unique map of the city’s ‘inner geography’ – the stories, knowledge, observations and alternative perspectives that pervade all places. This human dimension of the city has so far been locked up inside the minds of its citizens. Deepmapper makes it publicly accessible for the first time.</h2>
-                <h2 className="subtitle">Launching in Autumn 2018, Deepmapper exemplifies technology at its most positive and important.</h2>
             </div>
         </section>
     </Fragment>
