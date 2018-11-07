1471da8c98c7383b53f288076ebdacf58884df14
Lara Inskip <larahy@gmail.com>
2018-11-07 08:09:24 -0500

    Move fetch place cast dependencies to home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,27 +1,46 @@
 import React, {Fragment} from 'react'
+import {connect} from 'react-redux'
+import PropTypes from 'prop-types'
+
 import FilterablePlacecastTiles from '../containers/Placecasts/FilterablePlacecastsContainer'
 import {fetchDependencies} from '../helpers/fetchDependencies'
 import {Dependencies, Headers} from '../constants/attributes'
 import {SimpleHeader} from '../components/Navigation/SimpleHeader'
 import './pages.scss'
 import HomepageFeedViewToggleContainer from '../containers/Placecasts/HomePageToggleContainer'
+import {getHomepageCurrentFeedView,} from '../selectors/placecasts'
 
 let HomePage = class extends React.Component {
     static displayName = 'Home'
+    static propTypes = {
+        currentView: PropTypes.string,
+    }
 
     render() {
+        const {currentView} = this.props
+
+        const mainElement = currentView === 'list'
+            ? <FilterablePlacecastTiles/>
+            : null
         return (
             <Fragment>
                 <SimpleHeader title={Headers.DEEPMAPPER}/>
-                <HomepageFeedViewToggleContainer />
-                <FilterablePlacecastTiles />
+                <HomepageFeedViewToggleContainer/>
+                {mainElement}
             </Fragment>
         )
     }
 }
 
+const mapStateToProps = (state) => {
+    return {
+        currentView: getHomepageCurrentFeedView(state),
+    };
+};
+
 HomePage = fetchDependencies([
+    Dependencies.PLACECASTS,
     Dependencies.EXPERTS
 ])(HomePage)
 
-export default HomePage
+export default connect(mapStateToProps)(HomePage)
