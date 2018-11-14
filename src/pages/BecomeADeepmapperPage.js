56e6d4517c1a9b27eac83e92629a33d27f6ae896
Lara Inskip <larahy@gmail.com>
2018-11-14 13:54:01 -0500

    Update text on about page and become a deep mapper page

--- src/pages/BecomeADeepmapperPage.js
+++ src/pages/BecomeADeepmapperPage.js
@@ -9,7 +9,40 @@ export default class BecomeADeepmapperPage extends React.Component {
         return (
             <Fragment>
                 <SimpleHeader title={Headers.BECOME_A_DEEPMAPPER}/>
-                <BecomeADeepmapperFormContainer />
+                <section className="section">
+                    <div className="container">
+                        <div className="content">
+                            <p>Are you a scholar, working in any field? Are you a published writer, in any genre? Or do
+                                you
+                                have
+                                a unique story to tell about a specific place? We welcome you to apply to become a
+                                Deepmapper
+                                and start publishing placecasts.</p>
+                            <br/>
+                            <p>Fill in the fields below, and we’ll review your application. We’ll get back to you within
+                                48
+                                hours by email - do ensure Deepmapper emails don’t go to your junk folder.</p>
+                            <br/>
+                            <p>Be sure to be accurate: what you write now becomes the basis for your Deepmapper public
+                                profile
+                                (though you’ll be able to edit at a later date). </p>
+                            <br/>
+                            <p>For the Bio section, summarise your experience, expertise and interests. Think of it as
+                                the
+                                way
+                                the user will get to know you. The Bio will be attached to every placecast you publish.
+                                Feel
+                                free to include links: to webpages or to your social media profiles. Please never
+                                include
+                                commercial links. Deepmapper is a non-commercial space.</p>
+                            <br/>
+                            <p>Thank you for your interest in Deepmapper. We very much look forward to hearing from
+                                you.</p>
+                        </div>
+                    </div>
+                </section>
+
+                <BecomeADeepmapperFormContainer/>
             </Fragment>
         )
     }
