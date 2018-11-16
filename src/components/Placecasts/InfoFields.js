a699a90787b57f75594e7e7e41a611d600f266f7
Lara Inskip <larahy@gmail.com>
2018-11-16 13:35:05 -0500

    Update search bar placeholder texts according to whether or not there exists an address already for that placecast

--- src/components/Placecasts/InfoFields.js
+++ src/components/Placecasts/InfoFields.js
@@ -32,7 +32,7 @@ export default class InfoFields extends React.Component {
                     name='title'
                     description='title'
                     type='text'
-                    placeholder='Placecast Title'
+                    placeholder='Type Placecast Title'
                     validations={[
                         {name: Validations.MANDATORY}
                     ]}
--- src/containers/Maps/NotifyingSearchBar.js
+++ src/containers/Maps/NotifyingSearchBar.js
@@ -1,11 +1,17 @@
 import {connect} from 'react-redux'
 import {selectPlacecastAddress} from '../../actions/create2'
 import SearchBar from '../../components/Maps/Search/SearchBar'
-
+import {getAddress} from '../../selectors/create'
+import {isEmpty} from 'lodash'
+export const mapStateToProps = state => {
+    return {
+        hasPreviousAddress: !isEmpty(getAddress(state))
+    }
+}
 export const mapDispatchToProps = dispatch => {
     return {
         onSelectAddress: address => dispatch(selectPlacecastAddress(address))
     }
 }
 
-export default connect(null, mapDispatchToProps)(SearchBar);
+export default connect(mapStateToProps, mapDispatchToProps)(SearchBar);
